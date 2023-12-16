a806f5b5fe47170f052015b02850826ccdbc8e97
Sebastian Markbage <sebastian@calyptus.eu>
2023-12-15 19:34:33 -0500

    stash

--- packages/react-server/src/ReactFlightServerConfigDebugNode.js
+++ packages/react-server/src/ReactFlightServerConfigDebugNode.js
@@ -10,6 +10,12 @@
 import {createAsyncHook, executionAsyncId} from './ReactFlightServerConfig';
 import {enableAsyncDebugInfo} from 'shared/ReactFeatureFlags';
 
+type TrackedNode = {
+  cause: null | TrackedNode,
+};
+
+const trackedNodes: Map<number, TrackedNode> = new Map();
+
 // Initialize the tracing of async operations.
 // We do this globally since the async work can potentially eagerly
 // start before the first request and once requests start they can interleave.
@@ -19,14 +25,30 @@ export function initAsyncDebugInfo(): void {
   if (__DEV__ && enableAsyncDebugInfo) {
     createAsyncHook({
       init(asyncId: number, type: string, triggerAsyncId: number): void {
-        // TODO
+        const trigger = trackedNodes.get(triggerAsyncId);
+        const node: TrackedNode = {
+          type: type === 'PROMISE' && executionAsyncId() !== triggerAsyncId ? 'AWAIT' : type,
+          stack: new Error(),
+          trigger: trigger,
+        };
+        trackedNodes.set(asyncId, node);
       },
       promiseResolve(asyncId: number): void {
-        // TODO
-        executionAsyncId();
+        const executionAsyncId = async_hooks.executionAsyncId();
+        if (asyncId !== executionAsyncId) {
+          const resolvedNode = nodes.get(asyncId);
+          const trigger = nodes.get(executionAsyncId);
+          if (resolvedNode && trigger) {
+            // Track the current execution scope as the true trigger
+            // since that was what ultimately resolved this promise.
+            resolvedNode.trigger = trigger;
+          }
+        }
       },
       destroy(asyncId: number): void {
-        // TODO
+        // We should either have the node in the graph already as a trigger
+        // of whatever is still remaining.
+        nodes.delete(asyncId);
       },
     }).enable();
   }
