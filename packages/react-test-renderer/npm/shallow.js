9fdad3af7b82c2393df9ac45a94f1c1e1747aae2
Jack Pope <jackpope@meta.com>
2024-02-29 11:32:36 +0100

    [RTR] Throw in shallow export

--- packages/react-test-renderer/npm/shallow.js
+++ packages/react-test-renderer/npm/shallow.js
@@ -1,3 +1,9 @@
 'use strict';
 
-module.exports = require('react-shallow-renderer');
+function ReactShallowRenderer() {
+  throw new Error(
+    'react-test-renderer is deprecated. To use react-test-renderer/shallow, import react-shallow-renderer directly. See https://react.dev/warnings/react-test-renderer'
+  );
+}
+
+module.exports = ReactShallowRenderer;
